a60d47ac87ddbf2d3522ef2a01dde5d605a4b3be
Daniel Ufeli <danielufeli@yahoo.com>
2022-02-11 17:14:19 +0100

    change app function to class

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { PureComponent, Fragment } from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
+class App extends PureComponent {
+  render() {
+    return (
+      <>
+        <Calculator />
+      </>
+    );
+  }
 }
 
 export default App;
